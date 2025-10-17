45e2903b9ed519af533a98c1d52f6ca2e2144734
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-17 20:40:41 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-autorizaciones.settings}
 export PORT=${PORT:-8080}
+export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-autorizaciones.settings}
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput || true
-python manage.py seed_defaults || true
+python manage.py seed_defaults || true  # crea sectores y superusuario si falta
 
 exec gunicorn autorizaciones.wsgi:application --bind 0.0.0.0:${PORT} --workers 3 --timeout 120
