4f7f8b5632791841afb1ba6b4ee073d936cdd100
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-17 19:55:01 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput || true
 python manage.py seed_defaults || true
 
-# Start gunicorn
 exec gunicorn autorizaciones.wsgi:application --bind 0.0.0.0:${PORT} --workers 3 --timeout 120
